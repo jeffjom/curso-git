4010cbaf9d0edd3a421bd6aef08754803bc26100
Jefferson Onofre Matavello <jeff_jom@hotmail.com>
2021-03-08 23:57:17 -0300

    Acrescentado um bom dia em Inglesh

--- Projeto_teste/Program.cs
+++ Projeto_teste/Program.cs
@@ -4,8 +4,8 @@ namespace Projeto_teste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Have a nice day!");
             Console.ReadLine();
         }
     }
